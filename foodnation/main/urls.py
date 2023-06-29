221e820cdf2336aabe0be5f5b069ee8c16fac27e
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-06-29 12:39:19 +0000

    add urls

---
+++ foodnation/main/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
+    path("sign-up", views.sign_up, name="sign_up"),
+    path("create-post", views.create_post, name="create_post"),
+]
