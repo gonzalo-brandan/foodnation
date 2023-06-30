698d0960839fd72c2ef24e55b4bafeb92f8ba632
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-06-30 17:31:12 +0000

    add items urls

--- foodnation/main/urls.py
+++ foodnation/main/urls.py
@@ -1,12 +1,15 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),
     #path("", views.home, name="home"),
+    path("items/", include("item.urls")),
     path("home", views.home, name="home"),
     path("sign-up", views.sign_up, name="sign_up"),
     path("create-post", views.create_post, name="create_post"),
     path("contact", views.contact, name="contact")
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
