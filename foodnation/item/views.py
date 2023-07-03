5af28b73278278649ca09e0c8337a1e42937f5ed
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-07-03 13:41:18 +0000

    add item details view

---
+++ foodnation/item/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Item
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=True).exclude(pk=pk)[0:3]
+
+    return render(request, 'item/details.html', {'item': item, 'related_items': related_items})
